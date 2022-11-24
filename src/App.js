dc3cdcaed82b7d2146a005879c7304f9bfa20325
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-11-24 14:19:15 +0900

    themes
    what is themes

--- src/App.js
+++ src/App.js
@@ -5,51 +5,16 @@ const Wrapper = styled.div`
   justify-content: center;
   height: 100vh;
   align-items: center;
+  background-color:${props => props.theme.backgroundColor}
+`;
+const Title = styled.h1`
+  color : ${props => props.theme.textColor};
 `;
 
-const rotationAnimation = keyframes`
-  0%{
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-  50%{
-    transform: rotate(360deg);
-    border-radius: 50px;
-  }
-  100%{
-    transform: rotate(0deg);
-    border-radius: 50px;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width 200px;
-  background-color: coral;
-  animation: ${rotationAnimation} 1s linear infinite;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  
-  ${Emoji}{ // DOM targeting
-    
-    &:hover{
-      font-size: 100px;
-    }
-  }
-`
 function App() {
   return (
     <Wrapper>
-      <Box>
-          <Emoji>
-            😂
-          </Emoji>
-      </Box>
+      <Title>Hello</Title>
     </Wrapper>
   );
 }
